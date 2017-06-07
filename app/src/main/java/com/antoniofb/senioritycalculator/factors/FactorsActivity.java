f18e579a0ed6c1e9ea412282ba2c18bf1ce4d85f
antoniofb3 <antoniofb3>
2017-06-06 22:00:12 -0500

    Adding listView to Factors activity

--- app/src/main/java/com/antoniofb/senioritycalculator/factors/FactorsActivity.java
+++ app/src/main/java/com/antoniofb/senioritycalculator/factors/FactorsActivity.java
@@ -2,14 +2,24 @@ package com.antoniofb.senioritycalculator.factors;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.antoniofb.senioritycalculator.R;
 
 public class FactorsActivity extends AppCompatActivity {
 
+    private ListView lvFactors;
+    private String[] factors = {"Formal Education", "Experience", "Management", "Communication", "Technical Skills", "Leadership Experience", "Empowerment"};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_factors);
+
+        ArrayAdapter adapter = new ArrayAdapter<String>(this,R.layout.activity_listview, factors);
+
+        lvFactors = (ListView) findViewById(R.id.factorsList);
+        lvFactors.setAdapter(adapter);
     }
 }
--- app/src/main/java/com/antoniofb/senioritycalculator/registration/RegistrationActivity.java
+++ app/src/main/java/com/antoniofb/senioritycalculator/registration/RegistrationActivity.java
@@ -1,11 +1,17 @@
 package com.antoniofb.senioritycalculator.registration;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.antoniofb.senioritycalculator.R;
+import com.antoniofb.senioritycalculator.factors.FactorsActivity;
 
 public class RegistrationActivity extends AppCompatActivity {
 
@@ -23,7 +29,31 @@ public class RegistrationActivity extends AppCompatActivity {
         etEmployeeName = (EditText) findViewById(R.id.employeeName);
         etEmployeeJob = (EditText) findViewById(R.id.employeeJob);
         etEmployeeSeniority = (EditText) findViewById(R.id.employeeSeniority);
+        btRegisterEmployee = (Button) findViewById(R.id.btnRegisterEmployee);
 
-        //etEmployeeSeniority.setOnEditorActionListener();
+        etEmployeeSeniority.setOnEditorActionListener(new TextView.OnEditorActionListener(){
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event){
+                if(actionId == EditorInfo.IME_ACTION_DONE){
+                    registerEmployee();
+                    return true;
+                }
+                return false;
+            }
+        });
+
+        btRegisterEmployee.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                registerEmployee();
+            }
+        });
+
+    }
+
+    public void registerEmployee(){
+        //if (formIsValidated()){
+            startActivity(new Intent(RegistrationActivity.this, FactorsActivity.class));
+        //}
     }
 }
